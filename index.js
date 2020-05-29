b76b7999b3007ab36c846e69b95967d59832571b
uqair <uqairali.zekiexperts.com>
2020-05-28 23:38:15 -0700

    adding middleware for the error handling

--- index.js
+++ index.js
@@ -5,46 +5,71 @@ const app = express()
 app.use(express.json())
 const port = 3002;
 
-//Send a GET request to /quotes to Read list of quotes.
-app.get('/quotes', async (req, res) => {
-    try {
-        const quotes = await records.getQuotes()
-        res.json(quotes)
-    } catch (err) {
-        res.json(err)
-    }
-
-})
-//Send GET request to /quotes/:id to Read single quote
-app.get('/quotes/:id', async (req, res) => {
-    try {
-        const quote = await records.getQuote(req.params.id)
-        res.json(quote)
-    } catch (err) {
-        res.json(err)
-    }
-
-})
-//Adding new quotes
-app.post('/quotes', async (req, res) => {
-    try {
-        if (!req.body.quote || !req.body.author)
-            return res.status(400).json({ message: "quote and author required." })
-        const data = {
-            quote: req.body.quote,
-            author: req.body.author
+function asyncHandler(callBack) {
+    return async(req, res, next) => {
+        try {
+            await callBack(req, res, next)
+        } catch (err) {
+            next(err)
         }
-        const quote = await records.createQuote(data)
-        if (quote)
-            res.status(201).json(quote)
-        else
-            res.status(404).json({ message: "Quote not found." })
-    } catch (err) {
-        res.status(500).json(err)
+    }
+}
+
+//Send a GET request to /quotes to Read list of quotes.
+app.get('/quotes', asyncHandler(async (req, res, next) => {
+    const quotes = await records.getQuotes()
+    res.json(quotes)
+
+}))
+//Send GET request to /quotes/:id to Read single quote
+app.get('/quotes/:id', asyncHandler(async (req, res, next) => {
+    const quote = await records.getQuote(req.params.id)
+    res.json(quote)
+}))
+//Adding new quotes
+app.post('/quotes', asyncHandler(async (req, res, next) => {
+    if (!req.body.quote || !req.body.author)
+        return res.status(400).json({ message: "quote and author required." })
+    const data = {
+        quote: req.body.quote,
+        author: req.body.author
+    }
+    const quote = await records.createQuote(data)
+    if (quote)
+        res.status(201).json(quote)
+    else
+        res.status(404).json({ message: "Quote not found." })
+}))
+
+//update quote to passing id
+app.put('/quotes/:id', asyncHandler(async (req, res, next) => {
+    const quote = await records.getQuote(req.params.id)
+    if (quote) {
+        quote.quote = req.body.quote;
+        quote.author = req.body.author;
+        await records.updateQuote(quote)
+        res.end()
+    } else {
+        res.status(404).json({ message: "Quote not found." })
     }
 
+}))
+//Delete request using id to delete quote
+app.delete('/quotes/:id', asyncHandler(async (req, res, next) => {
+    const quote = await records.getQuote(req.params.id)
+    await records.deleteQuote(quote)
+    res.status(204).end()
+}))
+//handle not fond request
+app.use((req, res, next) => {
+    const err = new Error("Not Found.");
+    err.status = 404;
+    next(err)
 })
-
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ message: err.message })
+})
 app.listen(port, (err => {
     if (err)
         console.log("failed to running server!")
